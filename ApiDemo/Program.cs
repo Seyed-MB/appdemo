41addaa75e3a741226ee16652846b574c66f5cb4
98937 <98937@LAPTOP-3DLHA77J>
2025-10-19 15:14:18 +0330

    hazfe gozaneye tablet

--- ApiDemo/Program.cs
+++ ApiDemo/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//var products = new List<string> { "لپتاپ", "موبایل", "کتاب" };
+var products = new List<string> { "لپتاپ", "موبایل", "کتاب", "تبلت" };
+
+app.MapGet("/products", () => products);
+
+app.MapPost("/products", (string newProduct) =>
+{
+    products.Add(newProduct);
+    return Results.Ok(newProduct);
+});
+
+app.Run("http://0.0.0.0:5000");
